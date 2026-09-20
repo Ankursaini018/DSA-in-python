14dea2776bc665a03202ab535ec4611f7290cec3
Ankur Saini <officialankur0707@gmail.com>
2026-09-20 10:43:30 +0530

    nested loop in nested list

---
+++ Arrays/nested.py
@@ -0,0 +1,10 @@
+classroom = [
+    [85, 90, 78, 92, 88], #r0
+    [75, 80, 70, 82, 78], #r1
+    [95, 98, 92, 96, 94]  #r2
+]
+
+for row in classroom:
+    for mark in row:
+        print(mark, end = " ")
+    print()    
--- Arrays/traversal.py
+++ Arrays/traversal.py
@@ -1,10 +1,10 @@
 ###left to right traversal
 marks = [10, 20, 30, 40, 50]
-# for mark in marks:
-#     print(mark)
+for mark in marks:
+    print(mark)
 
-# for i in range(len(marks)):
-#     print(marks[i])    
+for i in range(len(marks)):
+    print(marks[i])    
 
 
 

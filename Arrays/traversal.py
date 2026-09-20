2b0877f380cee5442853d262e75a14a986c7998b
Ankur Saini <officialankur0707@gmail.com>
2026-09-20 10:15:24 +0530

    Traversal pattern

---
+++ Arrays/traversal.py
@@ -0,0 +1,22 @@
+###left to right traversal
+marks = [10, 20, 30, 40, 50]
+# for mark in marks:
+#     print(mark)
+
+# for i in range(len(marks)):
+#     print(marks[i])    
+
+
+
+### right to left traversal
+
+for mark in reversed(marks):
+    print(mark)
+
+
+for mark in marks[::-1]:
+    print(mark)
+
+
+for i in range(len(marks)-1, -1, -1):
+    print(marks[i])

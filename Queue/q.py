62351de5b18081a3356e898407107647533038b8
Ankur Saini <officialankur0707@gmail.com>
2026-09-22 16:36:03 +0530

    Queue : enqueue and dequeue

---
+++ Queue/q.py
@@ -0,0 +1,13 @@
+queue = []
+
+#enqueue
+queue.append("Rishika")
+queue.append("Ankur")
+queue.append("Amit")
+queue.append("prachi")
+print(queue)
+
+#dequeue
+first = queue.pop(0)
+print(first)
+print(queue)

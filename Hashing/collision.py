37e4f3228f0f0cd06b3df7edd44c1ee7d6bb06ba
Ankur Saini <officialankur0707@gmail.com>
2026-09-24 16:49:11 +0530

    Hashing: Collision

---
+++ Hashing/collision.py
@@ -0,0 +1,13 @@
+users = {}
+
+#insert
+users["Ankur@gmail.com"] = "Ankur saini"
+users["John@gmail.com"] = "John Doe"
+users["Rishika@gmail.com"] = "Rishika gour"
+
+#key = Ankur@gmail.com
+#value = Ankur saini
+#box = has(key)% total_boxes
+#store (box, "Ankur saini")
+
+print(hash("Ankur@gmail.com"))

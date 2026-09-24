a154a81c6bef048c86f90b4dd3a65b29045f3a6a
Ankur Saini <officialankur0707@gmail.com>
2026-09-24 16:37:51 +0530

    Hashing: set

---
+++ Hashing/set.py
@@ -0,0 +1,8 @@
+online_users = set()
+
+online_users.add("Alice")
+online_users.add("Bob")
+online_users.add("Charlie")
+
+print("Online users:", online_users)
+print("Alice" in online_users)  # Check if Alice is online  
